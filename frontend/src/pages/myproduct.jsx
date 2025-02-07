856d9dacbc8f5e7d25df684820059a01ee5d4c79
Vrajpatel4021 <vraj.patel.s73@kalvium.community>
2025-02-07 11:28:57 +0530

    Milestone 14 completed

--- frontend/src/pages/myproduct.jsx
+++ frontend/src/pages/myproduct.jsx
@@ -36,7 +36,7 @@ export default function MyProducts() {
     }
 
     return (
-        <div className="w-full min-h-screen bg-neutral-800">
+        <div className="w-full min-h-screen bg-black">
             <h1 className="text-3xl text-center text-white py-6">My products</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
                 {products.map((product) => (
