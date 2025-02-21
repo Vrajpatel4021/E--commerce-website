352cb70074c1b7b9cd9ac2d4c442ccee0aed1a37
Vrajpatel4021 <vraj.patel.s73@kalvium.community>
2025-02-21 17:23:06 +0530

    Milestone 19 completed

--- frontend/src/components/auth/myproduct.jsx
+++ frontend/src/components/auth/myproduct.jsx
@@ -1,50 +1,43 @@
 import React, { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
-import axios from 'axios' 
+import PropTypes from "prop-types";
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios';
 
-const Myproduct=({_id,name,images,description,price})=>{
-    const [currentIndex,setCurrentIndex] = useState(0)
-    const navigate = useNavigate();
+const Myproducts=({_id,name,images,description,price})=>{
+    const[currentIndex,setCurrentIndex]=useState(0)
+    const navigate=useNavigate();
 
     useEffect(()=>{
-        if(!images||images.length===0){
-            return "No images";
-        }
-
-        const interval = setInterval(()=>{
-
+        if(!images||images.length ===0) return "No Images";
+        const interval=setInterval(() => {
             setCurrentIndex((prevIndex)=>(prevIndex+1)%images.length)
-
+            
         },2000);
-
         return ()=>clearInterval(interval)
     },[images])
-    const currentImage = images && images.length>0 ? images[currentIndex]:null;
 
+    const handleEdit=()=>{
+        navigate(`/create-product/${_id}`)
+    }
 
-
-
-    const handleEdit = () => {
-        navigate(`/create-product/${_id}`);
-    };
-    const handleDelete = async () => { //Add this line for M14
-        try {
-            const response = await axios.delete(
-                `http://localhost:8000/api/v2/product/delete-product/${_id}`
-            );
-            if (response.status === 200) {
-                alert("Product deleted successfully!");
-                // Reload the page or fetch products again
+    const handleDelete=async ()=>{
+        try{
+            const response=await axios.delete(`http://localhost:8000/api/v2/product/delete-product/${_id}`);
+            if(response.status===200){
+                alert("Product Deleted");
                 window.location.reload();
             }
-        } catch (err) {
-            console.error("Error deleting product:", err);
-            alert("Failed to delete product.");
+        }catch(err){
+            console.error(`Error deleting product:${err}`);
+            alert("Falied to delete the product")
         }
-    };
+
+    }
+
+    const currentImage = images && images.length > 0 ? images[currentIndex] : null;
 
     return (
-        <div className="bg-teal-700 p-4 rounded-lg shadow-md flex flex-col justify-between">
+        <div className="bg-neutral-200 p-4 rounded-lg shadow-md flex flex-col justify-between">
             <div className="w-full">
                 {currentImage && (
                     <img
@@ -63,13 +56,13 @@ const Myproduct=({_id,name,images,description,price})=>{
                     onClick={handleEdit}
                 >
                     Edit
-                </button> 
+                </button>
                 <button
-                    className="w-full text-white px-4 py-2 rounded-md bg-red-700 mt-2 hover:bg-neutral-700 transition duration-300"
+                    className="w-full text-white px-4 py-2 rounded-md bg-red-500 hover:bg-red-400 transition duration-300"
                     onClick={handleDelete}
                 >
                     Delete
-                </button> 
+                </button>
             </div>
         </div>
     );
@@ -77,6 +70,4 @@ const Myproduct=({_id,name,images,description,price})=>{
 
 
 
-
-
-export default Myproduct;
+export default Myproducts;
