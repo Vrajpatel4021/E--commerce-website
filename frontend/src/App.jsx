d8ca60a41ff3bb5cdd205c47bd43478f29b49b99
Vrajpatel4021 <vraj.patel.s73@kalvium.community>
2025-04-26 02:37:55 +0530

    Final change 14

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import {
-  LoginPage,
-  SignupPage,
-  Home,
-  CreateProduct,
-  MyProducts,
-  Cart,
-  ProductDetails,
-  Profile,
-  CreateAddress,
-  SelectAddress,
-  OrderConfirmation,
-  MyOrdersPage
-} from "./Routes";
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,  SignupPage, Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress,OrderConfirmation,MyOrdersPage} from "./Routes";
 import "./App.css";
 
 const App = () => {
-  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
-
-  // Protected Route Wrapper
-  const RequireAuth = ({ children }) => {
-    return isLoggedIn ? children : <Navigate to="/login" replace />;
-  };
-
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-
-        {/* Protected Routes */}
-        <Route path="/" element={<RequireAuth><Home /></RequireAuth>} />
-        <Route path="/create-product" element={<RequireAuth><CreateProduct /></RequireAuth>} />
-        <Route path="/create-product/:id" element={<RequireAuth><CreateProduct /></RequireAuth>} />
-        <Route path="/my-products" element={<RequireAuth><MyProducts /></RequireAuth>} />
-        <Route path="/cart" element={<RequireAuth><Cart /></RequireAuth>} />
-        <Route path="/product/:id" element={<RequireAuth><ProductDetails /></RequireAuth>} />
-        <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
-        <Route path="/create-address" element={<RequireAuth><CreateAddress /></RequireAuth>} />
-        <Route path="/select-address" element={<RequireAuth><SelectAddress /></RequireAuth>} />
-        <Route path="/order-confirmation" element={<RequireAuth><OrderConfirmation /></RequireAuth>} />
-        <Route path="/myorders" element={<RequireAuth><MyOrdersPage /></RequireAuth>} />
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/create-product" element={<CreateProduct />} />
+      <Route path="/create-product/:id" element={<CreateProduct />} />
+      <Route path="/my-products" element={<MyProducts/>} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/create-address" element={<CreateAddress />} />
+      <Route path='/select-address' element={<SelectAddress/>}/>
+      <Route path="/order-confirmation" element={<OrderConfirmation/>}/>
+      <Route path="/myorders" element={<MyOrdersPage/>}/>
+    </Routes>
     </BrowserRouter>
-  );
-};
+    
+  )
+}
 
-export default App;
+export default App
